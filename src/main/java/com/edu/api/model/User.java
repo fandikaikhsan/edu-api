5823b715dd8f72a2136a5f40b101b83f2be29738
Fandika <fandika.ikhsan@gmail.com>
2024-08-09 14:48:40 +0700

    fix: add getter setter lombok solve empty return

--- src/main/java/com/edu/api/model/User.java
+++ src/main/java/com/edu/api/model/User.java
@@ -1,9 +1,13 @@
 package com.edu.api.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
